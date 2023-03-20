ca331324b657f2d97a770aafaf6cf0d085349fd3
Braden Evans <123213094+bradene0@users.noreply.github.com>
2023-03-20 15:11:36 -0500

    Commented in code snippet from scrambled-meggs

--- public/main.js
+++ public/main.js
@@ -135,4 +135,25 @@ function animate() {
 }
 
 animate() 
+ 
+/* From scrambled-meggs repo
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
 
+document.querySelector("h1").onmouseover = event => {
+   let iterations = 0;
+
+   const interval = setInterval(() => {
+    event.target.innerText = event.target.innerText.split("")
+    .map((letter, index) => {
+        if(index < iterations) {
+            return event.target.dataset.value[index];
+        }
+    
+    return letters[Math.floor(Math.random() * 26)]
+    })
+    .join('');
+    if(iterations >= event.target.dataset.value.length) clearInterval(interval);
+    iterations += 1 / 3;
+   }, 33);
+}
+*/
